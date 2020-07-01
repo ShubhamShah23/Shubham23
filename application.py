0038eca8a4fc8384dd4eb1edd071961459b6609c
ShubhamShah23 <67577031+ShubhamShah23@users.noreply.github.com>
2020-07-01 21:14:05 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
+        delete = request.form.get("delete")
+        if delete:
+            del notes[delete]
+        else:
+            pass
+    return render_template("index.html", notes=notes)
